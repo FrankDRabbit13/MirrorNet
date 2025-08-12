588497ce1ef61aae14b4930b7591bedc12fcf8d2
mirror <mirror@mirrornet.net>
2025-08-12 09:48:38 +0000

    hmm

---
+++ .idx/dev.nix
@@ -0,0 +1,35 @@
+nix
+{ pkgs, ... }: {
+
+  # NOTE: This is an excerpt of a complete Nix configuration example.
+  # For more information about the dev.nix file in Firebase Studio, see
+  # https://firebase.google.com/docs/studio/customize-workspace
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+        # Optionally, specify a directory that contains your web app
+        # cwd = "app/client";
+      };
+      # The following object sets Android previews
+      # Note that this is supported only on Flutter workspaces
+      android = {
+        manager = "flutter";
+      };
+    };
+  };
+
+  # OTHER CONFIGURATION...
+}
